047f45d19629706e814fec640a44582fbf564087
James Harmon <jamesharmon@gmail.com>
2014-06-05 23:11:54 -0500

    Update toolchain and generate additional html files

--- Labs/runtoolchain.sh
+++ Labs/runtoolchain.sh
@@ -1,2 +1,9 @@
-
 asciidoctor lab-01.adoc -o lab-01.html
+asciidoctor lab-02.adoc -o lab-02.html
+asciidoctor lab-03.adoc -o lab-03.html
+asciidoctor lab-04.adoc -o lab-04.html
+asciidoctor lab-05.adoc -o lab-05.html
+asciidoctor lab-06.adoc -o lab-06.html
+asciidoctor lab-07.adoc -o lab-07.html
+asciidoctor lab-08.adoc -o lab-08.html
+
